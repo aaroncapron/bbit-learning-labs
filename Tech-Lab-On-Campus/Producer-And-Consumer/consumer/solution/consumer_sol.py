a4a727b79ed5fa866dd5eaa3f99863ce05826337
Aaron <159083424+aaroncapron@users.noreply.github.com>
2024-04-18 18:57:36 -0600

    consumercode progress

---
+++ Tech-Lab-On-Campus/Producer-And-Consumer/consumer/solution/consumer_sol.py
@@ -0,0 +1,59 @@
+import pika
+import os
+
+from consumer_interface import mqConsumerInterface
+
+class mqConsumer(mqConsumerInterface):
+    def __init__(self,binding_key: str, exchange_name: str, queue_name: str)-> None:
+        
+        self.binding_key = binding_key
+        self.exchange_name = exchange_name
+        self.queue_name = queue_name
+        self.setupRMQCOnnection()
+        pass
+
+    def setupRMQConnection(self) -> None:
+        # Set-up Connection to RabbitMQ service
+        con_params = pika.URLParameters(os.environ["AMQP_URL"])
+        connection = pika.BlockingConnection(parameters=con_params)
+        # Establish Channel
+        channel = connection.channel()
+        # Create Queue if not already present
+        channel.queue_declare(queue="Queue Name")
+        # Create the exchange if not already present
+        exchange = channel.exchange_declare(exchange="Exchange Name")
+        # Bind Binding Key to Queue on the exchange
+        channel.queue_bind(
+            queue= "Queue Name",
+            routing_key= "Routing Key",
+            exchange="Exchange Name",
+        )
+        # Set-up Callback function for receiving messages
+        channel.basic_consume(
+            "Queue Name", self.on_message_callback, auto_ack=False
+        )
+        pass
+
+    def on_message_callback(
+        self, channel, method_frame, header_frame, body
+    ) -> None:
+        # Acknowledge message
+        channel.basic_ack(method_frame.delivery_tag, False)
+        #Print message (The message is contained in the body parameter variable)
+        print(body)
+        pass
+
+    def startConsuming(self) -> None:
+        # Print " [*] Waiting for messages. To exit press CTRL+C"
+
+        # Start consuming messages
+        pass
+    
+    def __del__(self) -> None:
+        # Print "Closing RMQ connection on destruction"
+        
+        # Close Channel
+
+        # Close Connection
+        
+        pass
